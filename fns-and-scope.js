8a81b526389e5c6ea27f557e77d441592a11e555
Gabriella <gaba@Gabriellas-MacBook-Pro.local>
2018-01-22 16:44:58 -0600

    updated complete

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -1,25 +1,27 @@
 //////////////////PROBLEM 1////////////////////
 
-var name = 'Tyler';
+
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
-
-  var isTyler = function(name) {
-    if (name === "Tyler") {
-      return true;
-    } else {
-      return false;
-    }
+function isTyler(name){
+  if(name === 'Tyler') {
+    return true;
+  } else {
+    return false;
   }
+}
 
 //////////////////PROBLEM 2////////////////////
 
 
 //Create a function called getName that uses prompt() to prompt the user for their name, then returns the name.
-function getName () {
-  var name = prompt("What's your name") 
-  return name;
+function getName(){
+  return prompt();
 }
+
+
+
+
 
 
 //////////////////PROBLEM 3////////////////////
@@ -28,12 +30,11 @@ function getName () {
 
 //Create a function called welcome that uses your getName function you created in the previous problem to get the users name,
 //then alerts "Welcome, " plus whatever the users name is.
+function welcome(){
+  let name = getName()
+  alert("Welcome, " + name);
+}
 
-  function welcome () {
-    var name = getName();
-    alert("Welcome, " + name);
-  
-  }
 
 
 //////////////////PROBLEM 4////////////////////
@@ -58,7 +59,7 @@ passed to function. */
 //What are all the falsy values in JavaScript and how do you check if something is falsy?
 
 
-  //Answer Here
+  //Null, empty string '', NaN, false, undefined Answer Here 
 
 
 
@@ -68,22 +69,21 @@ passed to function. */
 
 //Create a function called myName that returns your name
 
-  var myName = function (name) {
+  let myName = () => {
     return "Gabriella";
   }
-  
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
-      var newMyName = myName;
+    let newMyName = myName;
   
 
   
 
 //Now alert the result of invoking newMyName
 
-      //alert(newMyName);
+      // alert(newMyName);
 
 
 //////////////////PROBLEM 7////////////////////
@@ -91,13 +91,13 @@ passed to function. */
 
 
 //Create a function called outerFn which returns an anonymous function which returns your name.
-function outerFn () {
-  return function anon () {
+
+let outerFn = () => {
+  return function anon() {
     return "Gabriella";
   }
 }
-outerFn();
-
+console.log(outerFn());
 
 
 
@@ -105,8 +105,8 @@ outerFn();
 
 
 //Now save the result of invoking outerFn into a variable called innerFn.
-var innerFn = outerFn();
 
+let innerFn = outerFn();
 
 //Now invoke innerFn.
-innerFn();
+innerFn;
